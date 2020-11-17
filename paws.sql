8e9b36be084f3374ad88ffb0ad7e799910ce8d0b
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-17 14:28:37 -0800

    Update paws.sql

--- paws.sql
+++ paws.sql
@@ -23,8 +23,8 @@ CREATE TABLE animal (
     last_foster_home INT(7),
     shelter_ID INT,
     wellness_check BOOL,
+    is_sheltered BOOL,
     PRIMARY KEY (animal_ID),
-    FOREIGN KEY (intake_ID) REFERENCES intake (intake_ID),
     FOREIGN KEY (last_foster_home) REFERENCES fosterhome (foster_ID),
     FOREIGN KEY (shelter_ID) REFERENCES shelter (shelter_ID)
 );
@@ -54,10 +54,10 @@ CREATE TABLE fosterhome_animal (
 
 -- customer
 CREATE TABLE customer (
-customer_ID INT NOT NULL,
-        customer_first_name VARCHAR(50) NOT NULL,
-        customer_last_name VARCHAR(50) NOT NULL,
-        customer_phone_ext INT(4) NOT NULL,
+    customer_ID INT NOT NULL,
+    customer_first_name VARCHAR(50) NOT NULL,
+    customer_last_name VARCHAR(50) NOT NULL,
+    customer_phone_ext INT(4) NOT NULL,
     customer_phone INT(10) NOT NULL,
     customer_street VARCHAR(50) NOT NULL,
     customer_city VARCHAR(50) NOT NULL,
@@ -65,81 +65,83 @@ customer_ID INT NOT NULL,
     customer_zip_code INT(5) NOT NULL,
     Last_contacted DATE NOT NULL,
     customer_email VARCHAR(255) NOT NULL,
-        PRIMARY KEY (customer_ID)
+    PRIMARY KEY (customer_ID)
 );
 
 -- fosterhome
 CREATE TABLE fosterhome (
     foster_ID INT NOT NULL,
-volunteer_ID INT NOT NULL,
-fosterhome_phone_ext INT(4) NOT NULL,
-        foster_phone INT(10) NOT NULL,
+    volunteer_ID INT NOT NULL,
+    fosterhome_phone_ext INT(4) NOT NULL,
+    foster_phone INT(10) NOT NULL,
     foster_street VARCHAR(50) NOT NULL,
     foster_city VARCHAR(50) NOT NULL,
     foster_state CHAR(2) NOT NULL,
     foster_zip_code INT(5) NOT NULL,
-        max_animals INT(10) NOT NULL,
-        PRIMARY KEY (foster_ID),
+    max_animals INT(10) NOT NULL,
+    PRIMARY KEY (foster_ID),
     FOREIGN KEY (volunteer_ID) REFERENCES volunteer (volunteer_ID)
 );
 
 -- volunteer
 CREATE TABLE volunteer (
     volunteer_ID INT(7) NOT NULL,
-        volunteer_first_name VARCHAR(50) NOT NULL,
-        volunteer_last_name VARCHAR(50) NOT NULL,
-        volunteer_phone_ext INT(4) NOT NULL,
-volunteer_phone INT(10) NOT NULL,
-volunteer_street VARCHAR(50) NOT NULL,
-volunteer_city VARCHAR(50) NOT NULL,
+    volunteer_first_name VARCHAR(50) NOT NULL,
+    volunteer_last_name VARCHAR(50) NOT NULL,
+    volunteer_phone_ext INT(4) NOT NULL,
+    volunteer_phone INT(10) NOT NULL,
+    volunteer_street VARCHAR(50) NOT NULL,
+    volunteer_city VARCHAR(50) NOT NULL,
     volunteer_state CHAR(2) NOT NULL,
     volunteer_zip_code INT(5),
-        Volunteer_email VARCHAR(255),
-        PRIMARY KEY (volunteer_ID)
+    volunteer_email VARCHAR(255),
+    PRIMARY KEY (volunteer_ID)
 );
+
 -- volunteer_shelter
 CREATE TABLE volunteer_shelter (
     volunteer_ID INT(7) NOT NULL,
-        shelter_ID INT(7) NOT NULL,
-        PRIMARY KEY (volunteer_ID, shelter_ID),
-        FOREIGN KEY (volunteer_ID) REFERENCES volunteer (volunteer_ID),
-        FOREIGN KEY (shelter_ID) REFERENCES shelter (shelter_ID)
+    shelter_ID INT(7) NOT NULL,
+    PRIMARY KEY (volunteer_ID, shelter_ID),
+    FOREIGN KEY (volunteer_ID) REFERENCES volunteer (volunteer_ID),
+    FOREIGN KEY (shelter_ID) REFERENCES shelter (shelter_ID)
 );
 
 -- staffmember_shelter
 CREATE TABLE staffmember_shelter (
     staffmember_ID INT NOT NULL,
-        shelter_ID INT NOT NULL,
-        PRIMARY KEY (staffmember_ID, shelter_ID),
-        FOREIGN KEY (staffmember_ID) REFERENCES staffmember (staffmember_ID),
-        FOREIGN KEY (shelter_ID) REFERENCES shelter (shelter_ID)
+    shelter_ID INT NOT NULL,
+    PRIMARY KEY (staffmember_ID, shelter_ID),
+    FOREIGN KEY (staffmember_ID) REFERENCES staffmember (staffmember_ID),
+    FOREIGN KEY (shelter_ID) REFERENCES shelter (shelter_ID)
 );
 
 
 -- staffmember
 CREATE TABLE staffmember (
     staffmember_ID INT NOT NULL,
-        PRIMARY KEY (staffmember_ID),
-staff_first_name VARCHAR(50) NOT NULL, 
-Staff_last_name VARCHAR(50) NOT NULL,
-Staff_phone INT NOT NULL, 
-staff_street VARCHAR(50) NOT NULL, 
-staff_city VARCHAR(50) NOT NULL, 
-Staff_state VARCHAR(50) NOT NULL,
-Staff_zipcode INT NOT NULL, 
-is_admin BOOL NOT NULL
+    PRIMARY KEY (staffmember_ID),
+    staff_first_name VARCHAR(50) NOT NULL, 
+    staff_last_name VARCHAR(50) NOT NULL,
+    staff_phone INT NOT NULL, 
+    staff_street VARCHAR(50) NOT NULL, 
+    staff_city VARCHAR(50) NOT NULL, 
+    staff_state VARCHAR(50) NOT NULL,
+    staff_zipcode INT NOT NULL, 
+    is_admin BOOL NOT NULL
 );
 
 -- shelter
 CREATE TABLE shelter (
     shelter_ID INT NOT NULL,
-        shelter_name VARCHAR(50) NOT NULL,
+    shelter_name VARCHAR(50) NOT NULL,
     shelter_phone INT NOT NULL,
-shelter_street VARCHAR(50) NOT NULL,
-shelter_city VARCHAR(50) NOT NULL,
+    shelter_street VARCHAR(50) NOT NULL,
+    shelter_city VARCHAR(50) NOT NULL,
     shelter_state VARCHAR(50) NOT NULL,
-        max_animals INT NOT NULL,
-        PRIMARY KEY (shelter_ID)
+    max_animals INT NOT NULL,
+    PRIMARY KEY (shelter_ID)
+);
 
 -- intake
 CREATE TABLE intake (
