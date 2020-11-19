cc672696c02a3e70aaf3d15253e893808c38c80f
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-19 15:47:50 -0800

    Update INSERT_shelter.sql

--- INSERT_shelter.sql
+++ INSERT_shelter.sql
@@ -1,58 +1,33 @@
-delete from shelter
+delete from shelter;
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (shelter_ID,'shelter_name', shelter_phone,' shelter_street','shelter_city','shelter_state',shelter_zip_code,max_animals)
--- another comment
+VALUES (1,'Happy Cats Animal Shelter', 5049999999,' 1234 Apple St.','Medford','OR',97504,60);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (1,'Happy Cats Animal Shelter', 5049999999,' 1234 Apple St.','Medford','OR',97504,60)
--- another comment
+VALUES (2,'Medford Animal Shelter', 6552426709,' 2334 Ribbon St.','Medford','OR',97504,60);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (2,'Medford Animal Shelter', 6552426709,' 2334 Ribbon St.','Medford','OR',97504,60)
--- another comment
+VALUES (3,'Southern Oregon Animal Rescue', 5551234567,' 3334 New Ave','Medford','OR',97501,120);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (3,'Southern Oregon Animal Rescue', 5551234567,' 3334 New Ave','Medford','OR',97501,120)
--- another comment
+VALUES (4,'Oregon Animal Sanctuary', 2099998888,' 7654 Main St.','Grants Pass','OR',97527,40);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (4,'Oregon Animal Sanctuary', 2099998888,' 7654 Main St.','Grants Pass','OR',97527,40)
--- another comment
+VALUES (5,'Pets R Us', 9095555555,' 3943 Jefferson St.','Jacksonville','OR',97530,15);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (5,'Pets R Us', 9095555555,' 3943 Jefferson St.','Jacksonville','OR',97530,15)
--- another comment
+VALUES (6,'Animal Savers', 6761448131,' 1235 Ant St.','Jacksonville','OR',97530,10);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (6,'Animal Savers', 6761448131,' 1235 Ant St.','Jacksonville','OR',97530,10)
--- another comment
+VALUES (7,'Petcompany', 7125316460,' 2335 Dog St.','Jacksonville','OR',97530,10);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (7,'Petcompany', 7125316460,' 2335 Dog St.','Jacksonville','OR',97530,10)
--- another comment
+VALUES (8,'Animals INC', 7489184789,' 3335 Cat Ave','Jacksonville','OR',97530,25);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (8,'Animals INC', 7489184789,' 3335 Cat Ave','Jacksonville','OR',97530,25)
--- another comment
+VALUES (9,'We Help Shelter', 7853053118,' 7777 lucky Dr.','Grants Pass','OR',97527,30);
 
--- a comment
 INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (9,'We Help Shelter', 7853053118,' 7777 lucky Dr.','Grants Pass','OR',97527,30)
--- another comment
-
--- a comment
-INSERT INTO shelter (shelter_ID, shelter_name, shelter_phone, shelter_street, shelter_city, shelter_state, shelter_zip_code, max_animals) 
-VALUES (10,'Cats the shelter', 8216921447,' 1234 John St.','Grants Pass','OR',97527,5)
--- another comment
+VALUES (10,'Cats the shelter', 8216921447,' 1234 John St.','Grants Pass','OR',97527,5);
 
 SELECT * FROM f20_paws.shelter;
