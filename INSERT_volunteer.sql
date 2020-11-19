7ffbedb70ff6995e0a62bd44e8b3f4e1bed2c3aa
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-19 15:49:23 -0800

    Update INSERT_volunteer.sql

--- INSERT_volunteer.sql
+++ INSERT_volunteer.sql
@@ -1,4 +1,3 @@
-select * from volunteer
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
 VALUES (34,'Barry','White',5411111234,'123 Circle St.','Medford','OR',97504, 'whiteb@workemail.com');
@@ -13,21 +12,21 @@ INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, 
 VALUES (37, 'Clifford', 'Hubbard'	,5411111237	,'859 5th St.'	,'Medford','OR',97504,'sasquatch88@outlook.com');
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
-VALUES (38, 'Daniel', 'Smith', 5411111238, '3458 6th St.', 'Medford', 'OR', 97504, 'funfunfun@email.com')
+VALUES (38, 'Daniel', 'Smith', 5411111238, '3458 6th St.', 'Medford', 'OR', 97504, 'funfunfun@email.com');
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
-VALUES (39,'Rob', 'John', 5411111239, '125 Circle St.', 'Medford', 'OR', 97504, 'thatis@email.com')
+VALUES (39,'Rob', 'John', 5411111239, '125 Circle St.', 'Medford', 'OR', 97504, 'thatis@email.com');
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
-VALUES (40,'Garland', 'Jimson', 5411111240, '126 Circle St.', 'Medford', 'OR', 97504, 'whoiswhat@yahoo.com')
+VALUES (40,'Garland', 'Jimson', 5411111240, '126 Circle St.', 'Medford', 'OR', 97504, 'whoiswhat@yahoo.com');
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
-VALUES (41, 'Ruth', 'Lund', 5411111241, '127 Circle St.', 'Medford', 'OR', 97504,  'Goaway@email.com')
+VALUES (41, 'Ruth', 'Lund', 5411111241, '127 Circle St.', 'Medford', 'OR', 97504,  'Goaway@email.com');
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
 VALUES ('42','Beth', 'Rund', '5411111242', '128 Circle St.', 'Medford', 'OR', '97504', 'goawayfromme@email.com');
 
 INSERT INTO volunteer (volunteer_ID, volunteer_first_name, volunteer_last_name, volunteer_phone, volunteer_street, volunteer_city, volunteer_state, volunteer_zip_code, volunteer_email)
-VALUES (43, 'Fritz', 'Fern', 5411111243, '129 Circle St.', 'Medford', 'OR', 97504, 'runmf@email.com')
+VALUES (43, 'Fritz', 'Fern', 5411111243, '129 Circle St.', 'Medford', 'OR', 97504, 'runmf@email.com');
 
-Delete from volunteer
+select * from volunteer
