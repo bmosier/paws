79741e2e4cb7643b0e6a3b3a2369a38bc5fee9cc
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-19 15:08:43 -0800

    Create INSERT_fosterhome.sql

---
+++ INSERT_fosterhome.sql
@@ -0,0 +1,31 @@
+select * from fosterhome
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (100, 34, 8675309, '123 Circle St.', 'Medford', 'Oregon', 97504, 2);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (200, 35, 8675311, '124 Circle St.', 'Medford', 'Oregon', 97504, 4);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (300, 36, 8675312, '321 Square St.', 'Medford', 'Oregon', 97504, 5);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (400, 37, 8675313, '859 5th St.', 'Medford', 'Oregon', 97504, 2);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (500, 38, 8675314, '3458 6th St.', 'Medford', 'Oregon', 97504, 6);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (600, 39, 8675315, '1234 Fun St.', 'Grants Pass', 'Oregon', 97526, 6);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (700, 40, 8675316, '1111 SW M St.', 'Grants Pass', 'Oregon', 97526, 6);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (800, 41, 8675317, '5555 Five St.', 'Grants Pass', 'Oregon', 97526, 6);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (900, 42, 8675318, '4568 NW N St.', 'Ashland', 'Oregon', 97502, 6);
+
+INSERT INTO fosterhome (foster_ID, volunteer_ID, foster_phone, foster_street, foster_city, foster_state, foster_zip_code, max_animals)
+VALUES (999, 43, 8675319, '7777 Seven St.', 'Ashland', 'Oregon', 97502, 6);
