df2434e78d3735ad7a350f0a90162922562a2c30
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-23 15:58:16 -0800

    Update INSERT_animal.sql
    added additional animal entries for adoption, must add them to intake record as well

--- INSERT_animal.sql
+++ INSERT_animal.sql
@@ -28,4 +28,34 @@ VALUES (108, 'Kicker', STR_TO_DATE('06-17-2020', '%m-%d-%Y'), 'Cat', 'Maine Coon
 INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
 VALUES (109, 'Ron', STR_TO_DATE('06-18-2020', '%m-%d-%Y'), 'Lizard', 'Iguana', 'M', TRUE, '2lbs green and dark green, excited and fun', 10, TRUE, FALSE, TRUE);
 
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (90, 'Larry', STR_TO_DATE('05-15-2006', '%m-%d-%Y'), 'Lizard', 'Salamander', 'M', TRUE, '.5 OZ green', 3, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (91, 'Sammy', STR_TO_DATE('06-15-2006', '%m-%d-%Y'), 'Dog', 'Great Dane', 'M', TRUE, 'lbs brown and white', 4, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (92, 'Bobby', STR_TO_DATE('07-15-2006', '%m-%d-%Y'), 'Dog', 'Black Labrador', 'F', TRUE, '45 lbs black, enthusiastic, cuddly', 5, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (93, 'Franky', STR_TO_DATE('08-15-2006', '%m-%d-%Y'), 'Dog', 'Pitbull', 'M', TRUE, '50 lbs lazy and territorial', 1, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (94, 'Lewy', STR_TO_DATE('09-15-2006', '%m-%d-%Y'), 'Dog', 'Australian Shepard', 'F', TRUE, 'small excited and enthusiastic 40 lbs', 1, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (95, 'Dewy', STR_TO_DATE('10-15-2006', '%m-%d-%Y'), 'Cat', 'Maine Coon', 'M', TRUE, '25 lbs timid and vocal', 2, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (96, 'Phillip', STR_TO_DATE('11-15-2006', '%m-%d-%Y'), 'Cat', 'Siamese', 'M', TRUE, '15 lbs friendly and fun', 2, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (97, 'Harry', STR_TO_DATE('12-15-2006', '%m-%d-%Y'), 'Cat', 'Calico', 'F', TRUE, '10 lbs black and white very energetic', 3, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (98, 'Barry', STR_TO_DATE('01-15-2007', '%m-%d-%Y'), 'Cat', 'Tabby', 'F', TRUE, '30 lbs overweight lethargic may require extra care', 4, TRUE, FALSE, TRUE);
+
+INSERT INTO animal (animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, animal_is_sterilized, animal_description, shelter_ID, wellness_check, is_fostered, is_sheltered)
+VALUES (99, 'Carrie', STR_TO_DATE('02-15-2007', '%m-%d-%Y'), 'Cat', 'Siamese', 'M', TRUE, '10 lbs fun and cuddly hairless', 7, TRUE, FALSE, TRUE);
+
 select * from animal;
