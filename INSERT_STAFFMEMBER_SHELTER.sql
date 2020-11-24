0f9e033fd4dc6f7517fc19584856b87b2dcadc25
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-23 16:25:42 -0800

    Create INSERT_STAFFMEMBER_SHELTER.sql

---
+++ INSERT_STAFFMEMBER_SHELTER.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050001, 1);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050002, 2);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050003, 3);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050004, 4);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050012, 5);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050014, 6);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050016, 7);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050017, 8);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050018, 9);
+
+INSERT INTO staffmember_shelter (staff_ID, shelter_ID)
+VALUES(9050019, 10);
+
+select * from staffmember_shelter;
