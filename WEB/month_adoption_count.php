23704b5a8651aa52de90a14cc2c48c41da3316be
zenon80 <49969640+zenon80@users.noreply.github.com>
2020-11-25 14:31:23 -0800

    Month Adoption Count HTML

--- WEB/month_adoption_count.php
+++ WEB/month_adoption_count.php
@@ -4,5 +4,21 @@
 		<title>Month Adoption Count</title>
 	</head>
 	<body>
+		<h2>Month Adoption Count (Stored Function)</h2>
+		<h4>Author: Denis Roman</h4>
+		<p><b>Description:</b> text here</p>
+		<p><b>Justification:</b> text here</p>
+		<p><b>Expected Execution:</b> text here</p>
+		
+		<form method="post">
+			<label for="date">Date: </label>
+			<input type="date" name="date" id="date"<?php
+if (isset($_POST['date'])) {
+	echo ' value="' . $_POST['date'] . '"';
+}
+?> />
+			<br />
+			<input type="submit" value="Submit" />
+		</form>
 	</body>
 </html>
