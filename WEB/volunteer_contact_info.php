aa1584d0e51129c81ef9ae1d6b067f9aa4053092
zenon80 <49969640+zenon80@users.noreply.github.com>
2020-11-24 17:12:05 -0800

    Updated volunteer_contact_info.php with basic html

--- WEB/volunteer_contact_info.php
+++ WEB/volunteer_contact_info.php
@@ -4,5 +4,13 @@
 		<title>Volunteer Contact Information</title>
 	</head>
 	<body>
+		<h2>Volunteer Contact Information (View)</h2>
+		<h4>Author: Denis Roman</h4>
+		<h4>Description</h4>
+		<p>text here</p>
+		<h4>Justification</h4>
+		<p>text here</p>
+		<h4>Expected Execution</h4>
+		<p>text here</p>
 	</body>
 </html>
