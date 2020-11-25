8c9b2d1f4b6d3ea477cdc54df132161ded978b0c
zenon80 <49969640+zenon80@users.noreply.github.com>
2020-11-25 14:49:29 -0800

    Month Adoption Count PHP/description paragraphs

--- WEB/month_adoption_count.php
+++ WEB/month_adoption_count.php
@@ -6,9 +6,14 @@
 	<body>
 		<h2>Month Adoption Count (Stored Function)</h2>
 		<h4>Author: Denis Roman</h4>
-		<p><b>Description:</b> text here</p>
-		<p><b>Justification:</b> text here</p>
-		<p><b>Expected Execution:</b> text here</p>
+		<p><b>Description:</b> This page allows you to view how many adoptions occurred in the month
+		of a specified date.</p>
+		<p><b>Justification:</b> Staff members, and perhaps the front-end of a user-accessible website,
+		can use this information to display a statistic of how many animals have been adopted so far this month,
+		as well as in any past month.</p>
+		<p><b>Expected Execution:</b> Upon inputting a date, the stored function will return a count of all adoptions that
+		occurred within the month of that date. For example, if you input 10/15/2020 as the date, a message should appear
+		stating that 10 adoptions occurred the month which 10/15/2020 is in.</p>
 		
 		<form method="post">
 			<label for="date">Date: </label>
@@ -20,5 +25,29 @@ if (isset($_POST['date'])) {
 			<br />
 			<input type="submit" value="Submit" />
 		</form>
+<?php
+require_once '../dbconfig-paws.php';
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set('display_errors', '1');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	
+	$date = $_POST['date'];
+	
+	$sql = new mysqli($hostname, $username, $password, $schema);
+	if ($sql->connect_errno) {
+		echo 'Connection failed: ' . $sql->connect_errno;
+	} else {
+		$sqlquery = "SELECT month_adoption_count(STR_TO_DATE('" . $sql->real_escape_string($date) . "', '%Y-%m-%d')) AS adoption_count;";
+		
+		$result = $sql->query($sqlquery);
+		
+		// result only has a single row
+		echo '<p>Adoption count for month of specified date: ' . ($result->fetch_assoc())['adoption_count'] . '</p>';
+		$result->free_result();
+		$sql->close();
+	}
+}
+?>
 	</body>
 </html>
