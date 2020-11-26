f4d9c7aab5f798d7b0f680a52c6337a56fa61073
Samgotdam <56000151+Samgotdam@users.noreply.github.com>
2020-11-25 22:30:54 -0800

    Update staffmember_volunteer_stats_report.php

--- WEB/staffmember_volunteer_stats_report.php
+++ WEB/staffmember_volunteer_stats_report.php
@@ -2,76 +2,66 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 <html>
 	<head>
-		<title>Animal Adoption Information</title>
+		<title>Staffmember and Volunteer Contact Report</title>
 	</head>
-
-	<body>
-		<h2>Animals currently available for adoption (View)</h2>
 		<div id="bar">
         		<div class="link"><a href="LandingPage.html">Landing Page</a></div>
             		<div class="link"><a href="PAWS.html">PAWS Homepage</a></div>
       		</div>
 
+	<body>
+		<h2>Customer Staff Statistics Report (Procedure)</h2>
 		<h4>Author: Samuel James</h4>
-		<h4>Description</h4>
-		<p>this is a view for all animal currently available for adoption</p>
-		<h4>Justification</h4>
-		<p>this allows customers to easily and quickly view all animals available for adoption</p>
-		<h4>Expected Execution</h4>
-		<p>All animals that have not been adopted shall be returned</p>
+		<p><b>Description:</b> This page allows shelter managers a way to view the amount of volunteers and staff members currently working for them and the staff or volunteer contact information</p>
+		<p><b>Justification:</b> This page is useful to shelter managers who need to view the amount of staff and volunteers they currently have working and how to contact them as well as being useful for staff to contact other staff members to cover shifts.</p>
+		<p><b>Expected Execution:</b> Upon inputting a shelter name the contact information for each staff member or volunteer working for the given shelter 
+		If the shelter name "Medford Animal Shelter" was input the information for all staff and volunteers tied to that shelter shall be displayed.</p>
+		<form method="post">
+			<label for="sheltername">Shelter Name: </label>
+			<input type="text" name="shelter_name" id="shelter_name"<?php
+if (isset($_POST['shelter_name'])) {
+	echo ' value="' . $_POST['shelter_name'] . '"';
+}
+?> />
+			<br />
+			<input type="submit" value="Submit" />
+		</form>
 <?php
-	require_once '/home/SOU/jamess1/dbconfig.php';
-	error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-	ini_set('display_errors', '1');
+require_once '../dbconfig.php';
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set('display_errors', '1');
 
-	 //animal_ID, name, animal_DOB, animal_species, animal_breed, animal_sex, 
-	//animal_is_sterilized, animal_description, Shelter_or_Foster_name, 
-	//city, street, state, phone, zipcode
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$shelter_name = $_POST['shelter_name'];
+	
 	$sql = new mysqli($hostname, $username, $password, $schema);
 	if ($sql->connect_errno) {
 		echo 'Connection failed: ' . $sql->connect_errno;
 	} else {
-	$result = $sql->query('SELECT * FROM v_animal_info;');
-	
+	$result = $sql->query("CALL staff_stats('" . $shelter_name . "');");
+		
 	echo '<table border="1" align="center">';
 	echo '<tr>';
-	echo '<th>Animal Id</th>';
-	echo '<th>Name</th>';
-	echo '<th>Animal Date of Birth</th>';
-	echo '<th>Breed</th>';
-	echo '<th>Animal Species</th>';
-	echo '<th>Gender</th>';
-	echo '<th>Shelter/Fosterhome name</th>';
-	echo '<th>Animal Brief Description</th>';
-	echo '<th>City</th>';
-	echo '<th>Street</th>';
-	echo '<th>State</th>';
-	echo '<th>phone</th>';
-	echo '<th>Zipcode</th>';
+	echo '<th>Staff/Volunteer First Name</th>';
+	echo '<th>Staff/Volunteer Last Name</th>';
+	echo '<th>Staff/Volunteer Phone Number</th>';
+	echo '<th>Staff/Volunteer Phone Ext or Email</th>';
+	echo '<th>Shelter Name</th>';
 	echo '</tr>';
-	
+
 	while ($row = $result->fetch_assoc()) {
 		echo '<tr>';
-		echo '<td>' . $row['animal_ID'] . '</td>';
-		echo '<td>' . $row['name'] . '</td>';
-		echo '<td>' . $row['animal_DOB'] . '</td>';
-		echo '<td>' . $row['animal_breed'] . '</td>';
-		echo '<td>' . $row['animal_species'] . '</td>';
-		echo '<td>' . $row['animal_sex'] . '</td>';
-		echo '<td>' . $row['Shelter_or_Foster_name'] . '</td>';
-		echo '<td>' . $row['animal_description'] . '</td>';
-		echo '<td>' . $row['city'] . '</td>';
-		echo '<td>' . $row['street'] . '</td>';
-		echo '<td>' . $row['state'] . '</td>';
+		echo '<td>' . $row['Firstname'] . '</td>';
+		echo '<td>' . $row['LastName'] . '</td>';
 		echo '<td>' . $row['phone'] . '</td>';
-		echo '<td>' . $row['zipcode'] . '</td>';
+		echo '<td>' . $row['email_or_phone_extension'] . '</td>';
+		echo '<td>' . $row['shelter_name'] . '</td>';
 		echo '</tr>';
 	}
-	
 	echo '</table>';
 	$result->free_result();
 	$sql->close();
+	}
 }
 ?>
 	</body>
